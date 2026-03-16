4756a4a00b506b7b9546daf3769d2fab85396aa1
terawuth <132336480+terawuthlogicspark@users.noreply.github.com>
2026-03-16 09:06:39 +0700

    feat: enhance webpack configuration to support custom pdfjs-dist module resolution

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-export default nextConfig;
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const nextConfig = {
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(/^pdfjs-dist$/, (resource) => {
+        resource.request = path.join(__dirname, './node_modules/pdfjs-dist/webpack.mjs')
+      })
+    )
+    return config
+  },
+}
+
+export default nextConfig
